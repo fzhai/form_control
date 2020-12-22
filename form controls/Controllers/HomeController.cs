c3ce19378b53d6232348b5112c7df6c40b4f04e1
fzhai <fzhai@yahoo.com>
2020-12-22 10:00:43 +0500

    Add files via upload

---
+++ form controls/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using form_controls.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace form_controls.Controllers
+{
+    public class HomeController : Controller
+    {
+        LibraryEntities DB = new LibraryEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var members = DB.members.ToList();
+            return View(members);
+        }
+    }
+}
